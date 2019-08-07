f66dd071ed2774748eea6df3da9fe558966af683
Piyush <gupta99piyush@gmail.com>
2019-08-08 03:27:38 +0530

    v3

--- js/pscript.js
+++ js/pscript.js
@@ -12,3 +12,40 @@
 //   console.log("a");
 // });
 // });
+function animate1(elementclass,animation1 )
+{
+var element3 = document.querySelectorAll(elementclass);
+var element4 = document.querySelector('.init');
+window.addEventListener('scroll', function(event) {
+  element3.forEach(element => {
+    var wintop     = $(window).scrollTop();
+    var winheight=$(window).height();
+    var winbottom= wintop+winheight;
+    console.log(winbottom);
+    const navheight=$(element4).height();
+    var eletop = $(element).offset().top;
+    var eleheight=$(element).height();
+    var elebottom=eletop + eleheight;
+    console.log(elebottom);
+      if(eletop>(wintop-eleheight) && elebottom<(winbottom+eleheight))
+     { element.style.opacity=1;
+      $(element).addClass(animation1); 
+      
+      
+      //  console.log("yes");
+    }
+     else
+     
+    {  
+   
+      element.style.opacity=0;
+       
+      $(element).removeClass(animation1);
+
+      //  console.log($(window).width());
+      //   console.log("no");
+      }
+
+})
+});
+}
