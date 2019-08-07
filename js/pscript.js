15bada8b0a5642d8cdcdb3c8156b1133d90ff2d3
Piyush <gupta99piyush@gmail.com>
2019-08-08 02:08:57 +0530

    v3

---
+++ js/pscript.js
@@ -0,0 +1,14 @@
+// var element1=document.querySelectorAll(".services");
+// var element2=document.querySelector(".each-service-logo");
+// var sheight=$(element2).height();
+// console.log(sheight);
+// console.log("a");
+// window.addEventListener('load',(event)=>{
+    
+
+// element1.forEach(element=>{
+  
+//   element.style.height=2*sheight;
+//   console.log("a");
+// });
+// });
