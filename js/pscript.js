a87d75e98b06533dacef2944f88ebdcd8939a808
Piyush <gupta99piyush@gmail.com>
2019-08-10 14:46:06 +0530

    v3.1

--- js/pscript.js
+++ js/pscript.js
@@ -12,6 +12,79 @@
 //   console.log("a");
 // });
 // });
+function heightset(class1,class2)
+{   $(document).ready(function(){
+
+
+     var element1=document.querySelectorAll(class1);
+    var element2=document.querySelector(class2);
+       element1.forEach((element)=>{
+        $(element).css('height',$(element2).height());
+        console.log($(element2).height());
+        console.log($(element).height());
+       })
+     
+    })
+     
+    
+}
+function heightset2(class1,class2)
+{   $(document).ready(function(){
+
+
+     var element1=document.querySelectorAll(class1);
+    var element2=document.querySelector(class2);
+       
+        $(element1).css('height',$(element2).height());
+        console.log($(element2).height());
+        console.log($(element).height());
+     
+     
+    })
+     
+    
+}
+// heightset(".crossfade-figure",".hometop");
+// heightset(".crossfade1-figure",".our-services");
+heightset2("figure",".whybest");
+heightset(".flipper",".each-service-logo");
+heightset(".flipcontainer",".each-service-logo");
+
+function marginset(class1,class2)
+{   $(document).ready(function(){
+
+
+     var element1=document.querySelectorAll(class1);
+    var element2=document.querySelector(class2);
+       element1.forEach((element)=>{
+        var eleh=$(element2).height();
+        $(element).css('margin-top',(-1*eleh));
+        console.log($(element2).height());
+        console.log($(element).css('margin-top'));
+       
+       })
+     
+    })
+     
+    
+}
+/* ....................................*/
+$(document).ready(()=>{
+    var element4=document.querySelector(".form-container");
+var element3=document.querySelector(".our-services");
+var element1=document.querySelector(".carousel-container");
+var element2=document.querySelector(".form-innercontainer");
+ $(element2).css('margin-top',-0.8*$(element2).height()); 
+$(element4).css('min-height',$(element2).height());
+// $(element4).css('margin-bottom',$(element2).height());
+
+ $(element3).css('margin-top',$(element4).height());
+
+
+})
+/* ....................................*/
+marginset(".each-service-desc-desc",".each-service-logo");
+
 function animate1(elementclass,animation1 )
 {
 var element3 = document.querySelectorAll(elementclass);
